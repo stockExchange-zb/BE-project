8ebfa1f09590b0d17ecd03fed4c97eaad4112dda
SaeRyung <sr1094@naver.com>
2025-08-14 03:25:24 +0900

    feat: REST API controller 구현

---
+++ src/main/java/com/stockexchange/domain/execution/controller/ExecutionController.java
@@ -0,0 +1,30 @@
+package com.stockexchange.domain.execution.controller;
+
+import com.stockexchange.domain.execution.dto.ExecutionResDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class ExecutionController {
+
+    @GetMapping("/{userId}/execution")
+    @Operation(summary = "체결 내역 전체 조회", description = "체결 내역 전체를 조회합니다.")
+    public ResponseEntity<List<ExecutionResDTO>> getAllExecutions(@PathVariable Long userId) {
+//        TODO
+        return ResponseEntity.ok().body(List.of());
+    }
+
+    @GetMapping("/{userId}/execution/{executionId}")
+    @Operation(summary = "체결 상세 조회", description = "특정 체결 내역을 상세 조회합니다.")
+    public ResponseEntity<ExecutionResDTO> getExecutionById(@PathVariable("userId") Long userId, @PathVariable("executionId") Long executionId) {
+//        TODO
+        return ResponseEntity.ok().body(new ExecutionResDTO());
+    }
+}

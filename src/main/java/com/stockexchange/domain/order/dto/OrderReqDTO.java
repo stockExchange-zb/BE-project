1f3be4d006f1940eea11d70ff840a14a76585ff1
SaeRyung <sr1094@naver.com>
2025-09-11 20:23:51 +0900

    chore: OrderReqDTO - 주문 등록 시 유효성 추가

--- src/main/java/com/stockexchange/domain/order/dto/OrderReqDTO.java
+++ src/main/java/com/stockexchange/domain/order/dto/OrderReqDTO.java
@@ -1,6 +1,8 @@
 package com.stockexchange.domain.order.dto;
 
 import com.stockexchange.domain.order.entity.OrderType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,9 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Data
 public class OrderReqDTO {
+    @Min(value = 1)
     private int orderCount;
+    @DecimalMin(value = "0.01")
     private BigDecimal orderPrice;
     private OrderType orderType;
     private Long stockId;
